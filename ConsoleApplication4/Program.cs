4f7b9930818b23b34491c4f93bfda9e14b105c94
antonius.hilman <antonius.hilman@bhinneka.com>
2016-12-07 10:56:30 +0700

    first commit

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first number: ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Input second number");
+            string b = Console.ReadLine();
+
+            double a_value, b_value;
+
+            if (double.TryParse(a, out a_value) && double.TryParse(b, out b_value))
+            {
+                Console.WriteLine("Result: {0}", (a_value * b_value));
+            }
+            else
+            {
+                Console.WriteLine("Not an integer!!");
+            }
+        }
+    }
+}
